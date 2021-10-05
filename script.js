148b8053fdb794bb3f0ee6e11d07d9aa71b48d86
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-10-05 23:02:43 +0700

    Add sirip bawah dan lingkaran

--- script.js
+++ script.js
@@ -161,14 +161,12 @@ function getSiripDepanAtasAntena() {
   for(let k=0;k<720-1; k++) {
     i.push(...iCalon.map(calon => (calon + (k*4))))
   }
-  // console.log(v)
   const c = []
   for(let k=0; k<i.length; k++) c.push(...[1, 1, 1])
   return {v, i, c}
 }
 
 function getSiripTengahAntena() {
-  
   const radius = 0.075
   const centerZ = 0.0
   const keliling = Math.PI * 2 * radius
@@ -178,10 +176,10 @@ function getSiripTengahAntena() {
     const degreeNextRad = (i + 1) * 0.5 / Math.PI
     const offsetY = 1
     const vNew = [
-      -1.2,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
-      1.2,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
-      1.2,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad),
-      -1.2,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad)
+      -1.1,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
+      1.1,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
+      1.1,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad),
+      -1.1,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad)
     ]
     v.push(...vNew)
   }
@@ -190,28 +188,93 @@ function getSiripTengahAntena() {
     const degreeNextRad = (i + 1) * 0.5 / Math.PI
     const offsetY = 1.9
     const vNew = [
-      -1.2,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
-      1.2,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
-      1.2,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad),
-      -1.2,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad)
+      -1.1,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
+      1.1,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
+      1.1,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad),
+      -1.1,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad)
     ]
     v.push(...vNew)
   }
-  // for(let i=0; i<360; i++) {
-  //   const degreeCurrRad = i * 0.5 / Math.PI
-  //   const degreeNextRad = (i + 1) * 0.5 / Math.PI
-  //   const offsetY = -0.4
-  //   const vNew = [
-  //     -2,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
-  //     -1.2,radius * Math.sin(degreeCurrRad) + offsetY,centerZ + radius * Math.cos(degreeCurrRad),
-  //     -1.2,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad),
-  //     -2,radius * Math.sin(degreeNextRad) + offsetY,centerZ + radius * Math.cos(degreeNextRad)
-  //   ]
-  //   v.push(...vNew)
-  // }
   const iCalon = [3,2,1,3,1,0];
   const i = []
   for(let k=0;k<720-1; k++) {
+    i.push(...iCalon.map(calon => (calon + (k*4))))
+  }
+  // console.log(v)
+  const c = []
+  for(let k=0; k<i.length; k++) c.push(...[1, 1, 1])
+  return {v, i, c}
+}
+
+function getBulatBesarAntena() {
+  const radius = 1.8
+  const centerZ = 0.0
+  const keliling = Math.PI * 2 * radius
+  const v = []
+  for(let i=0; i<360; i++) {
+    const degreeCurrRad = i * 0.5 / Math.PI
+    const degreeNextRad = (i + 1) * 0.5 / Math.PI
+    const offsetY = 1.73
+    const vNew = [
+      radius * Math.sin(degreeCurrRad), offsetY + radius * Math.cos(degreeCurrRad),-0.4,
+      radius * Math.sin(degreeCurrRad), offsetY + radius * Math.cos(degreeCurrRad), -0.5,
+      radius * Math.sin(degreeNextRad), offsetY + radius * Math.cos(degreeNextRad), -0.5,
+      radius * Math.sin(degreeNextRad), offsetY + radius * Math.cos(degreeNextRad),-0.4
+    ]
+    v.push(...vNew)
+  }
+  const iCalon = [3,2,1,3,1,0];
+  const i = []
+  for(let k=0;k<360; k++) {
+    i.push(...iCalon.map(calon => (calon + (k*4))))
+  }
+  // console.log(v)
+  const c = []
+  for(let k=0; k<i.length; k++) c.push(...[1, 1, 1])
+  return {v, i, c}
+}
+
+function getBawahBulatBesarAntena() {
+  const radius = 0.4
+  const centerZ = 0.0
+  const keliling = Math.PI * 2 * radius
+  const v = [
+    1.2, -0.5, -0.4,
+    1.5, -0.5, -0.4,
+    1.5, -0.4, -0.5,
+    1.2, -0.4, -0.5,
+    1.0, -0.8, -0.8,
+    1.5, -0.8, -0.8,
+    1.5, -0.9, -0.9,
+    1.0, -0.9, -0.9,
+    -1.2, -0.5, -0.4,
+    -1.5, -0.5, -0.4,
+    -1.5, -0.4, -0.5,
+    -1.2, -0.4, -0.5,
+    -1.0, -0.8, -0.8,
+    -1.5, -0.8, -0.8,
+    -1.5, -0.9, -0.9,
+    -1.0, -0.9, -0.9,
+  ]
+  for(let i=0; i<22; i++) {
+    const degreeCurrRad = i * 0.5 / Math.PI
+    const degreeNextRad = (i + 1) * 0.5 / Math.PI
+    const offsetY = -0.4
+    const vNew = [
+      1.5 + radius * Math.sin(degreeCurrRad), -0.5 - radius * Math.sin(degreeCurrRad / 2),-0.4 - radius * Math.sin(degreeCurrRad / 2),
+      1.5 + radius * Math.sin(degreeCurrRad), -0.5 - radius * Math.sin(degreeCurrRad / 2), -0.5 - radius * Math.sin(degreeCurrRad / 2),
+      1.5 + radius * Math.sin(degreeNextRad), -0.4 - radius * Math.sin(degreeNextRad / 2), -0.5 - radius * Math.sin(degreeCurrRad / 2),
+      1.5 + radius * Math.sin(degreeNextRad), -0.4 - radius * Math.sin(degreeNextRad / 2),-0.4 - radius * Math.sin(degreeCurrRad / 2),
+      -(1.5 + radius * Math.sin(degreeCurrRad)), -0.5 - radius * Math.sin(degreeCurrRad / 2),-0.4 - radius * Math.sin(degreeCurrRad / 2),
+      -(1.5 + radius * Math.sin(degreeCurrRad)), -0.5 - radius * Math.sin(degreeCurrRad / 2), -0.5 - radius * Math.sin(degreeCurrRad / 2),
+      -(1.5 + radius * Math.sin(degreeNextRad)), -0.4 - radius * Math.sin(degreeNextRad / 2), -0.5 - radius * Math.sin(degreeCurrRad / 2),
+      -(1.5 + radius * Math.sin(degreeNextRad)), -0.4 - radius * Math.sin(degreeNextRad / 2),-0.4 - radius * Math.sin(degreeCurrRad / 2)
+    ]
+    v.push(...vNew)
+  }
+  const iCalon = [3,2,1,3,1,0];
+  const i = [3,2,1,3,1,0]
+  for(let k=1;k<180; k++) {
     i.push(...iCalon.map(calon => (calon + (k*4))))
   }
   // console.log(v)
@@ -247,6 +310,8 @@ pushToVerticesAndIndices(vertices, indices, colors, getWhiteBatangAntena())
 pushToVerticesAndIndices(vertices, indices, colors, getAlasAntena())
 pushToVerticesAndIndices(vertices, indices, colors, getSiripDepanAtasAntena())
 pushToVerticesAndIndices(vertices, indices, colors, getSiripTengahAntena())
+pushToVerticesAndIndices(vertices, indices, colors, getBulatBesarAntena())
+pushToVerticesAndIndices(vertices, indices, colors, getBawahBulatBesarAntena())
 // pushToVerticesAndIndices(vertices, indices, getLeftSideRedAntena())
 // pushToVerticesAndIndices(vertices, indices, getRightSideRedAntena())
 
